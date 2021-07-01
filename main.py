f6f549bfbe2baddb6da357a95cc1050ebae5781a
Sofia Broomé <sbroome@kth.se>
2021-07-01 08:50:56 +0200

    runs with diving48 datamodule

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import torch
-import torchvision
 from torch import nn
-import torch.functional as F
 import pytorch_lightning as pl
 
 import utils
-from transforms_video import *
-from data_loader_av import VideoFolder
+from data_module import Diving48DataModule
 from models.convlstm import StackedConvLSTMModel
 
 
@@ -45,49 +42,6 @@ class ConvLSTMModule(pl.LightningModule):
         y_hat = self(x)
         return {'loss': self.loss_function(y_hat, y)}
 
-    def train_dataloader(self):
-        upscale_size_train = int(config['input_spatial_size'] * config["upscale_factor_train"])
-        transform_train_pre = ComposeMix([
-            [RandomRotationVideo(15), "vid"],
-            [Scale(upscale_size_train), "img"],
-            [RandomCropVideo(config['input_spatial_size']), "vid"],
-        ])
-
-        # Transforms common to train and eval sets and applied after "pre" transforms
-        transform_post = ComposeMix([
-            [torchvision.transforms.ToTensor(), "img"],
-            [torchvision.transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],  # default values for imagenet
-                std=[0.229, 0.224, 0.225]), "img"]
-        ])
-
-        train_val_data = VideoFolder(root=config['data_folder'],
-                                     json_file_input=config['json_data_train'],
-                                     json_file_labels=config['json_file_labels'],
-                                     clip_size=config['clip_size'],
-                                     nclips=config['nclips_train_val'],
-                                     step_size=config['step_size_train_val'],
-                                     is_val=False,
-                                     transform_pre=transform_train_pre,
-                                     transform_post=transform_post,
-                                     augmentation_mappings_json=config['augmentation_mappings_json'],
-                                     augmentation_types_todo=config['augmentation_types_todo'],
-                                     get_item_id=True,
-                                     seq_first=self.seq_first
-                                     )
-        train_data, val_data = torch.utils.data.random_split(
-            train_val_data, [config['nb_train_samples'], config['nb_val_samples']],
-            generator=torch.Generator().manual_seed(42))
-
-        print(" > Using {} processes for data loader.".format(
-            config["num_workers"]))
-
-        return torch.utils.data.DataLoader(
-            train_data,
-            batch_size=config['batch_size'], shuffle=True,
-            num_workers=config['num_workers'], pin_memory=True,
-            drop_last=True)
-
     def configure_optimizers(self):
         return torch.optim.SGD(self.parameters(), config['lr'],
                                momentum=config['momentum'],
@@ -96,8 +50,10 @@ class ConvLSTMModule(pl.LightningModule):
 
 if __name__ == '__main__':
     trainer = pl.Trainer(fast_dev_run=True)
+    dm = Diving48DataModule(data_dir=config['data_folder'], config=config)
+    # trainer = pl.Trainer(max_epochs=config['num_epochs'],
+    #                      progress_bar_refresh_rate=1)
     input_tensor = torch.rand(5, 6, 3, 224, 224)
-    convlstm = ConvLSTMModule(input_size=input_tensor.size(), hidden_per_layer=[3, 3, 3],
+    conv_lstm = ConvLSTMModule(input_size=input_tensor.size(), hidden_per_layer=[3, 3, 3],
                               kernel_size_per_layer=[5, 5, 5])
-    trainer.fit(convlstm)
-    print(convlstm(input_tensor))
+    trainer.fit(conv_lstm, dm)
