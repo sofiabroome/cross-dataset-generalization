97d07aa14ddd156c79bd963336c60c20db99fe45
Sofia Broomé <sbroome@kth.se>
2021-07-01 16:33:09 +0200

    add val and test step, start explore logging and ckpt

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import torch
 from torch import nn
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning import seed_everything
+from pytorch_lightning.loggers import WandbLogger
 
 import utils
+import torchmetrics
 from data_module import Diving48DataModule
 from models.convlstm import StackedConvLSTMModel
-
 
 # load configurations
 args = utils.load_args()
@@ -25,6 +28,7 @@ class ConvLSTMModule(pl.LightningModule):
         self.linear = nn.Linear(
             in_features=self.t * self.c * int(self.h / 2**self.num_layers) * int(self.w/2**self.num_layers),
             out_features=48)
+        self.accuracy = torchmetrics.Accuracy()
 
     def forward(self, x) -> torch.Tensor:
         x = self.convlstm_encoder(x)
@@ -42,6 +46,29 @@ class ConvLSTMModule(pl.LightningModule):
         y_hat = self(x)
         return {'loss': self.loss_function(y_hat, y)}
 
+    def validation_step(self, batch, batch_idx):
+        x, y, item_id = batch
+        y_hat = self(x)
+        preds = torch.argmax(y_hat, dim=1)
+        acc = self.accuracy(preds, y)
+        self.log('val_acc', acc, prog_bar=True)
+        return {'val_loss': self.loss_function(y_hat, y),
+                'val_acc': acc}
+
+    def test_step(self, batch, batch_idx):
+        x, y, item_id = batch
+        y_hat = self(x)
+        preds = torch.argmax(y_hat, dim=1)
+        acc = self.accuracy(preds, y)
+        self.log('test_acc', acc, prog_bar=True)
+        return {'test_loss': self.loss_function(y_hat, y)}
+
+    def validation_epoch_end(self, outputs) -> None:
+        val_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        val_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+        log = {'avg_val_loss': val_loss}
+        return {'log': log, 'val_loss': val_loss, 'val_acc': val_acc}
+
     def configure_optimizers(self):
         return torch.optim.SGD(self.parameters(), config['lr'],
                                momentum=config['momentum'],
@@ -49,11 +76,19 @@ class ConvLSTMModule(pl.LightningModule):
 
 
 if __name__ == '__main__':
-    trainer = pl.Trainer(fast_dev_run=True)
+    # wandb_logger = WandbLogger(project='cross-dataset-generalization', config=config)
+    # trainer = pl.Trainer(fast_dev_run=True)
+    # seed_everything(42, workers=True)
+    # trainer = pl.Trainer()
     dm = Diving48DataModule(data_dir=config['data_folder'], config=config)
-    # trainer = pl.Trainer(max_epochs=config['num_epochs'],
-    #                      progress_bar_refresh_rate=1)
+    # checkpoint_callback = ModelCheckpoint(monitor='val_acc')
+    trainer = pl.Trainer(max_epochs=config['num_epochs'])
+                         # progress_bar_refresh_rate=1,
+                         # callbacks=[checkpoint_callback],
+                         # limit_train_batches=5,
+                         # logger=wandb_logger)
     input_tensor = torch.rand(5, 6, 3, 224, 224)
     conv_lstm = ConvLSTMModule(input_size=input_tensor.size(), hidden_per_layer=[3, 3, 3],
                               kernel_size_per_layer=[5, 5, 5])
     trainer.fit(conv_lstm, dm)
+    # trainer.test(conv_lstm, datamodule=dm)
