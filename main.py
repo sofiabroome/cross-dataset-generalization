13cf3cc3a311aa9427b3ef7040d77cdce25947a5
Sofia Broomé <x_sofbr@berzelius001.eth.cluster>
2021-07-04 08:34:16 +0200

    get loss acc in separate function. ignore lightning logs

--- main.py
+++ main.py
@@ -44,30 +44,38 @@ class ConvLSTMModule(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y, item_id = batch
         y_hat = self(x)
-        return {'loss': self.loss_function(y_hat, y)}
+        loss, acc = self.get_loss_acc(y_hat, y)
+        self.log('train_acc', acc, prog_bar=True)
+        self.log('train_loss', loss, prog_bar=True)
+        return {'loss': loss}
 
     def validation_step(self, batch, batch_idx):
         x, y, item_id = batch
         y_hat = self(x)
-        preds = torch.argmax(y_hat, dim=1)
-        acc = self.accuracy(preds, y)
+        loss, acc = self.get_loss_acc(y_hat, y)
         self.log('val_acc', acc, prog_bar=True)
-        return {'val_loss': self.loss_function(y_hat, y),
-                'val_acc': acc}
+        self.log('val_loss', loss)
+        return {'val_loss': loss, 'val_acc': acc}
 
     def test_step(self, batch, batch_idx):
         x, y, item_id = batch
         y_hat = self(x)
+        loss, acc = self.get_loss_acc(y_hat, y)
+        self.log('test_acc', acc, prog_bar=True)
+        self.log('test_loss', loss)
+        return {'test_loss': loss}
+
+    def get_loss_acc(self, y_hat, y):
+        loss = self.loss_function(y_hat, y)
         preds = torch.argmax(y_hat, dim=1)
         acc = self.accuracy(preds, y)
-        self.log('test_acc', acc, prog_bar=True)
-        return {'test_loss': self.loss_function(y_hat, y)}
+        return loss, acc
 
-    def validation_epoch_end(self, outputs) -> None:
-        val_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        val_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
-        log = {'avg_val_loss': val_loss}
-        return {'log': log, 'val_loss': val_loss, 'val_acc': val_acc}
+    # def validation_epoch_end(self, outputs) -> None:
+    #     val_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+    #     val_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+    #     log = {'avg_val_loss': val_loss}
+    #     return {'log': log, 'val_loss': val_loss, 'val_acc': val_acc}
 
     def configure_optimizers(self):
         return torch.optim.SGD(self.parameters(), config['lr'],
@@ -76,19 +84,26 @@ class ConvLSTMModule(pl.LightningModule):
 
 
 if __name__ == '__main__':
-    # wandb_logger = WandbLogger(project='cross-dataset-generalization', config=config)
+    wandb_logger = WandbLogger(project='cross-dataset-generalization', config=config)
+    # trainer = pl.Trainer(fast_dev_run=True, gpus=1, accelerator='ddp')
+    # trainer = pl.Trainer(fast_dev_run=True, gpus=1)
     # trainer = pl.Trainer(fast_dev_run=True)
     # seed_everything(42, workers=True)
     # trainer = pl.Trainer()
     dm = Diving48DataModule(data_dir=config['data_folder'], config=config)
-    # checkpoint_callback = ModelCheckpoint(monitor='val_acc')
-    trainer = pl.Trainer(max_epochs=config['num_epochs'])
-                         # progress_bar_refresh_rate=1,
-                         # callbacks=[checkpoint_callback],
-                         # limit_train_batches=5,
-                         # logger=wandb_logger)
-    input_tensor = torch.rand(5, 6, 3, 224, 224)
-    conv_lstm = ConvLSTMModule(input_size=input_tensor.size(), hidden_per_layer=[3, 3, 3],
+    checkpoint_callback = ModelCheckpoint(monitor='val_acc', verbose=True,
+                                          dirpath='xdataset_output/',
+                                          filename='{epoch}-{val_loss:.2f}-{val_acc:.2f}')
+    trainer = pl.Trainer(max_epochs=config['num_epochs'], gpus=1,
+                       progress_bar_refresh_rate=1,
+                       callbacks=[checkpoint_callback],
+                       weights_save_path='xdataset_output/',
+                       logger=wandb_logger)
+    conv_lstm = ConvLSTMModule(input_size=(5, 16, 3, 224, 224), hidden_per_layer=[32, 32, 32],
                               kernel_size_per_layer=[5, 5, 5])
     trainer.fit(conv_lstm, dm)
     # trainer.test(conv_lstm, datamodule=dm)
+    # trainer.test(datamodule=dm,
+    #              ckpt_path='xdataset_output/cross-dataset-generalization/1gtp4f1a/checkpoints/epoch=0-val_loss=3.30-val_acc=0.10.ckpt')
+
+    
