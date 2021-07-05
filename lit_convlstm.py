c81b0d3b07afb2478e36b1e60736405e41d2087f
Sofia Broomé <sbroome@kth.se>
2021-07-05 14:08:17 +0200

    separate main from litmodule, rm train

---
+++ lit_convlstm.py
@@ -0,0 +1,73 @@
+from models.convlstm import StackedConvLSTMModel
+import pytorch_lightning as pl
+from torch import nn
+import torchmetrics
+import torch
+
+
+class ConvLSTMModule(pl.LightningModule):
+    def __init__(self, input_size, hidden_per_layer, kernel_size_per_layer, conv_stride,
+                 lr, momentum, weight_decay):
+        super(ConvLSTMModule, self).__init__()
+
+        self.b, self.t, self.c, self.h, self.w = input_size
+        self.seq_first = True
+        self.num_layers = len(hidden_per_layer)
+        self.lr = lr
+        self.momentum = momentum
+        self.weight_decay = weight_decay
+        self.convlstm_encoder = StackedConvLSTMModel(
+            self.c, hidden_per_layer, kernel_size_per_layer, conv_stride)
+        self.flatten = nn.Flatten(start_dim=1, end_dim=-1)
+        self.linear = nn.Linear(
+            in_features=self.t * hidden_per_layer[-1] * int(self.h /(2**self.num_layers*conv_stride)) *
+                        int(self.w/(2**self.num_layers*conv_stride)), out_features=48)
+        self.accuracy = torchmetrics.Accuracy()
+        self.save_hyperparameters()
+
+    def forward(self, x) -> torch.Tensor:
+        x = self.convlstm_encoder(x)
+        x = self.flatten(x)
+        x = self.linear(x)
+        return x
+
+    def loss_function(self, y_hat, y):
+        criterion = nn.CrossEntropyLoss()
+        loss = criterion(y_hat, y)
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        x, y, item_id = batch
+        y_hat = self(x)
+        loss, acc = self.get_loss_acc(y_hat, y)
+        self.log('train_acc', acc, prog_bar=True)
+        self.log('train_loss', loss, prog_bar=True)
+        return {'loss': loss}
+
+    def validation_step(self, batch, batch_idx):
+        x, y, item_id = batch
+        y_hat = self(x)
+        loss, acc = self.get_loss_acc(y_hat, y)
+        self.log('val_acc', acc, prog_bar=True)
+        self.log('val_loss', loss)
+        return {'val_loss': loss, 'val_acc': acc}
+
+    def test_step(self, batch, batch_idx):
+        x, y, item_id = batch
+        y_hat = self(x)
+        loss, acc = self.get_loss_acc(y_hat, y)
+        self.log('test_acc', acc, prog_bar=True)
+        self.log('test_loss', loss)
+        return {'test_loss': loss}
+
+    def get_loss_acc(self, y_hat, y):
+        loss = self.loss_function(y_hat, y)
+        preds = torch.argmax(y_hat, dim=1)
+        acc = self.accuracy(preds, y)
+        return loss, acc
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.parameters(), self.lr,
+                               momentum=self.momentum,
+                               weight_decay=self.weight_decay)
+
--- main.py
+++ main.py
@@ -1,94 +1,23 @@
 import os
-import torch
-from torch import nn
 import pytorch_lightning as pl
+
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-from pytorch_lightning import seed_everything
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.plugins import DDPPlugin
+from pytorch_lightning import seed_everything
 
 import utils
 import argparse
-import torchmetrics
 from data_module import Diving48DataModule
-from models.convlstm import StackedConvLSTMModel
+from lit_convlstm import ConvLSTMModule
 
 
-class ConvLSTMModule(pl.LightningModule):
-    def __init__(self, input_size, hidden_per_layer, kernel_size_per_layer, conv_stride):
-        super(ConvLSTMModule, self).__init__()
-
-        self.b, self.t, self.c, self.h, self.w = input_size
-        self.seq_first = True
-        self.num_layers = len(hidden_per_layer)
-        self.convlstm_encoder = StackedConvLSTMModel(
-            self.c, hidden_per_layer, kernel_size_per_layer, conv_stride)
-        self.flatten = nn.Flatten(start_dim=1, end_dim=-1)
-        self.linear = nn.Linear(
-            in_features=self.t * hidden_per_layer[-1] * int(self.h /(2**self.num_layers*conv_stride)) *
-            int(self.w/(2**self.num_layers*conv_stride)), out_features=48)
-        self.accuracy = torchmetrics.Accuracy()
-        self.save_hyperparameters() 
-
-    def forward(self, x) -> torch.Tensor:
-        x = self.convlstm_encoder(x)
-        x = self.flatten(x)
-        x = self.linear(x)
-        return x
-
-    def loss_function(self, y_hat, y):
-        criterion = nn.CrossEntropyLoss()
-        loss = criterion(y_hat, y)
-        return loss
-
-    def training_step(self, batch, batch_idx):
-        x, y, item_id = batch
-        y_hat = self(x)
-        loss, acc = self.get_loss_acc(y_hat, y)
-        self.log('train_acc', acc, prog_bar=True)
-        self.log('train_loss', loss, prog_bar=True)
-        return {'loss': loss}
-
-    def validation_step(self, batch, batch_idx):
-        x, y, item_id = batch
-        y_hat = self(x)
-        loss, acc = self.get_loss_acc(y_hat, y)
-        self.log('val_acc', acc, prog_bar=True)
-        self.log('val_loss', loss)
-        return {'val_loss': loss, 'val_acc': acc}
-
-    def test_step(self, batch, batch_idx):
-        x, y, item_id = batch
-        y_hat = self(x)
-        loss, acc = self.get_loss_acc(y_hat, y)
-        self.log('test_acc', acc, prog_bar=True)
-        self.log('test_loss', loss)
-        return {'test_loss': loss}
-
-    def get_loss_acc(self, y_hat, y):
-        loss = self.loss_function(y_hat, y)
-        preds = torch.argmax(y_hat, dim=1)
-        acc = self.accuracy(preds, y)
-        return loss, acc
-
-    # def validation_epoch_end(self, outputs) -> None:
-    #     val_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-    #     val_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
-    #     log = {'avg_val_loss': val_loss}
-    #     return {'log': log, 'val_loss': val_loss, 'val_acc': val_acc}
-
-    def configure_optimizers(self):
-        return torch.optim.SGD(self.parameters(), config['lr'],
-                               momentum=config['momentum'],
-                               weight_decay=config['weight_decay'])
-
-
-if __name__ == '__main__':
+def main():
     # load configurations
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', '-c', help='json config file path')
-    parser.add_argument('--eval_only', '-e', action='store_true', 
+    parser.add_argument('--eval_only', '-e', action='store_true',
                         help="evaluate trained model on validation data.")
     parser.add_argument('--resume', '-r', action='store_true',
                         help="resume training from a given checkpoint.")
@@ -105,11 +34,14 @@ if __name__ == '__main__':
 
     seed_everything(42, workers=True)
 
-    conv_lstm = ConvLSTMModule(input_size=(config['batch_size'], config['clip_size'], 3,
-                               config['input_spatial_size'], config['input_spatial_size']),
-                               hidden_per_layer=config['hidden_per_layer'],
-                               kernel_size_per_layer=config['kernel_size_per_layer'],
-                               conv_stride=config['conv_stride'])
+    if config['model_name'] == 'lit_convlstm':
+        conv_lstm = ConvLSTMModule(input_size=(config['batch_size'], config['clip_size'], 3,
+                                               config['input_spatial_size'], config['input_spatial_size']),
+                                   hidden_per_layer=config['hidden_per_layer'],
+                                   kernel_size_per_layer=config['kernel_size_per_layer'],
+                                   conv_stride=config['conv_stride'],
+                                   lr=config['lr'], momentum=config['momentum'],
+                                   weight_decay=config['weight_decay'])
 
     checkpoint_callback = ModelCheckpoint(monitor='val_acc', mode='max',
                                           verbose=True,
@@ -140,6 +72,8 @@ if __name__ == '__main__':
 
     # trainer.test(conv_lstm, datamodule=dm)
     # trainer.test(datamodule=dm, model=conv_lstm,
-    #              ckpt_path='xdataset_output/None/epoch=1-val_loss=3.47-val_acc=0.07.ckpt')
+    #              ckpt_path=config['ckpt_path'])
 
-    
+
+if __name__ == '__main__':
+    main()
