9f3ca11947da87e19629446beb7c74fc1de1c9a8
Sofia Broomé <sbroome@kth.se>
2021-07-05 15:18:06 +0200

    syncdist, top5, lossfunc static

--- lit_convlstm.py
+++ lit_convlstm.py
@@ -20,9 +20,13 @@ class ConvLSTMModule(pl.LightningModule):
             self.c, hidden_per_layer, kernel_size_per_layer, conv_stride)
         self.flatten = nn.Flatten(start_dim=1, end_dim=-1)
         self.linear = nn.Linear(
-            in_features=self.t * hidden_per_layer[-1] * int(self.h /(2**self.num_layers*conv_stride)) *
-                        int(self.w/(2**self.num_layers*conv_stride)), out_features=48)
+            in_features=self.t * hidden_per_layer[-1] *
+            int(self.h /(2**self.num_layers*conv_stride)) *
+            int(self.w/(2**self.num_layers*conv_stride)),
+            out_features=48)
         self.accuracy = torchmetrics.Accuracy()
+        self.top5_accuracy = torchmetrics.Accuracy(top_k=5)
+        self.softmax = nn.Softmax(dim=1)
         self.save_hyperparameters()
 
     def forward(self, x) -> torch.Tensor:
@@ -31,7 +35,8 @@ class ConvLSTMModule(pl.LightningModule):
         x = self.linear(x)
         return x
 
-    def loss_function(self, y_hat, y):
+    @staticmethod
+    def loss_function(y_hat, y):
         criterion = nn.CrossEntropyLoss()
         loss = criterion(y_hat, y)
         return loss
@@ -39,7 +44,7 @@ class ConvLSTMModule(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y, item_id = batch
         y_hat = self(x)
-        loss, acc = self.get_loss_acc(y_hat, y)
+        loss, acc, _ = self.get_loss_acc(y_hat, y)
         self.log('train_acc', acc, prog_bar=True)
         self.log('train_loss', loss, prog_bar=True)
         return {'loss': loss}
@@ -47,24 +52,27 @@ class ConvLSTMModule(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         x, y, item_id = batch
         y_hat = self(x)
-        loss, acc = self.get_loss_acc(y_hat, y)
-        self.log('val_acc', acc, prog_bar=True)
-        self.log('val_loss', loss)
+        loss, acc, top5_acc = self.get_loss_acc(y_hat, y)
+        self.log('val_acc', acc, prog_bar=True, sync_dist=True)
+        self.log('val_top5_acc', top5_acc, sync_dist=True)
+        self.log('val_loss', loss, sync_dist=True)
         return {'val_loss': loss, 'val_acc': acc}
 
     def test_step(self, batch, batch_idx):
         x, y, item_id = batch
         y_hat = self(x)
-        loss, acc = self.get_loss_acc(y_hat, y)
-        self.log('test_acc', acc, prog_bar=True)
-        self.log('test_loss', loss)
+        loss, acc, top5_acc = self.get_loss_acc(y_hat, y)
+        self.log('test_acc', acc, prog_bar=True, sync_dist=True)
+        self.log('test_top5_acc', top5_acc, sync_dist=True)
+        self.log('test_loss', loss, sync_dist=True)
         return {'test_loss': loss}
 
     def get_loss_acc(self, y_hat, y):
         loss = self.loss_function(y_hat, y)
         preds = torch.argmax(y_hat, dim=1)
         acc = self.accuracy(preds, y)
-        return loss, acc
+        top5_acc = self.top5_accuracy(self.softmax(y_hat), y)
+        return loss, acc, top5_acc
 
     def configure_optimizers(self):
         return torch.optim.SGD(self.parameters(), self.lr,
